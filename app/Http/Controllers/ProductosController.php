1b737b125e71b5664edcb0a08a6cd4aec73a3282
alexgomez31 <agpan007@gmail.com>
2024-03-19 16:55:06 -0500

    cambios en el welcome

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -22,8 +22,8 @@ class ProductosController extends Controller
         return view('productos.createProd');
     }
 
-    
-   
+
+
 
     // public function store(Request $request)
     // {
@@ -36,10 +36,10 @@ class ProductosController extends Controller
     //         'ancho' => 'required|string',
     //         'plano' => 'file|mimes:pdf|max:2048',
     //     ]);
-    
+
     //     // Procesar la carga del PDF
     //     $planoPath = $request->file('plano')->store('public/pdfs');
-    
+
     //     // Crear un nuevo producto en la base de datos
     //     $producto = new Producto();
     //     $producto->tipoproducto = $request->tipoproducto;
@@ -50,7 +50,7 @@ class ProductosController extends Controller
     //     // Asignar la ruta del archivo al campo 'plano'
     //     $producto->plano = str_replace('public/', '', $planoPath);
     //     $producto->save();
-    
+
     //     // Redireccionar al usuario a la lista de productos con un mensaje de éxito
     //     return redirect()->route('product.index')->with('success', 'Producto creado correctamente');
     // }
@@ -99,7 +99,7 @@ class ProductosController extends Controller
     //         // Alert::success('xD','404');
     //         // return redirect('/producto');
     //         return response()->json(['error' => 'Este producto no contiene ningún archivo de soporte.']);
-    
+
     //     }
     //     $pdfUrl = storage_path('app/public/' . $producto->plano);
     //     return response()->file($pdfUrl);
@@ -120,7 +120,7 @@ class ProductosController extends Controller
     return response()->file($pdfPath);
 }
 
-    
+
 
 
 
@@ -158,35 +158,35 @@ class ProductosController extends Controller
             'ancho' =>'required|string',
             'plano' => 'nullable|file|mimes:pdf|max:2048',
         ]);
-    
+
         // Si se proporciona un nuevo PDF, eliminar el PDF anterior
         if ($request->hasFile('plano')) {
             Storage::delete('public/' . $producto->plano);
             $planoPath = $request->file('plano')->store('public/pdfs');
             $producto->plano = str_replace('public/', '', $planoPath);
         }
-    
+
         // Actualizar los demás campos del producto
         $producto->update($request->all());
-    
+
         return redirect()->route('product.index')->with('success', 'Producto actualizado correctamente');
     }
-    
+
 
 
     public function destroy(Producto $producto)
     {
         // Obtener la ruta del archivo PDF asociado al producto
         $pdfPath = storage_path('app/public/' . $producto->plano);
-    
+
         // Eliminar el producto
         $producto->delete();
-    
+
         // Si la ruta del archivo PDF existe, eliminarlo del sistema de archivos
         if (file_exists($pdfPath)) {
             unlink($pdfPath);
         }
-    
+
         return response()->json(['success' => 'Producto eliminado satisfactoriamente']);
     }
 
