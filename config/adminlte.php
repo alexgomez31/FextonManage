e4c66b53bbe1a9933c1729269018159e1624195e
alexgomez31 <agpan007@gmail.com>
2024-03-20 07:49:21 -0500

    icono casa

--- config/adminlte.php
+++ config/adminlte.php
@@ -196,7 +196,7 @@ return [
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
-    'classes_sidebar' => 'sidebar-dark-primary elevation-4',
+    'classes_sidebar' => 'sidebar-dark-success elevation-4',
     'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-white navbar-light',
     'classes_topnav_nav' => 'navbar-expand',
@@ -305,9 +305,6 @@ return [
             'topnav_right' => true,
         ],
 
-
-
-
         // Sidebar items:
         [
             'type' => 'sidebar-menu-search',
@@ -321,7 +318,7 @@ return [
         [
             'text' => 'Dashboard',
             'url' => 'dashboard',
-            'icon' => 'fa-solid fa-house',
+            'icon' => 'fa fa-home',
             'label_color' => 'success',
         ],
         [
